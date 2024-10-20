63a326ab7adf4b0488ea18fa4cfa16745a136a79
mohamed-jilani <mohamedjilani551@gmail.com>
2024-10-20 15:23:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+// src/App.js
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline text-center">
+        Hello, Tailwind in React!
+      </h1>
+      <p className="mt-4 text-gray-600 text-lg">
+        C'est ta première application avec Tailwind CSS et React.
+      </p>
     </div>
   );
 }
